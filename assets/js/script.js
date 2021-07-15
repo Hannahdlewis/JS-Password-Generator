dac858260e4a4c530e61273285ee8299bc6b2f34
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-14 22:10:53 -0400

    deleted notes at bottom of js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,51 +62,7 @@ function generatePassword() {
   return password
 }
 
-
-
-// when you click on the button it prompts you to..
-
-// {<button type="button" onclick='document.getElementById("generate").innerHTML = "Hello JavaScript!"'>generatePassword</button> */}
-
-
-
-// use a prompt to determine the lenfth of the password
-//between 8 and 128 characters
-
-
-// passwordLength = prompt('Please select a password length with a minimum of 8 characters and a maximum of 128 characters: ');
-// passwordLength = parseInt(passwordLength)
-
-//Continue to ask the question if user fails to meet password length criteria
-
-
-
-// PasswordArray created by running a random selector over the password character selelction array
-// in a loop of the password length
-
-// var passwordArray = [];
-// var i = 0;
-// while (i<passwordLength) {
-//   var randomChar = passCharSelectionArray[Math.floor(Math.random()*passCharSelectionArray.length)];
-//   passwordArray.push(randomChar);
-//   i++;
-// }
-
-// Password being returned to the function call in the form of a string
-// This requires a conversion from an array to a string
-
-// var passwordString = passwordArray.join("");
-
-// output password
-
-// return passwordString;{
-
-// }
-
-
 function getUserChoice(characterSet) {
-  // promt user to determine if using character set
-  var userChoice = prompt('Do you wish for your password to contain ' + characterSet + '? Please enter "Y" OR "N"');
 
 }
 
