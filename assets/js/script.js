f366f8f585af047f94d60c9a57122f358474374b
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-14 21:59:31 -0400

    finalized js

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,9 +52,9 @@ function generatePassword() {
 
   var password = ""
   for(var i = 0; i < passwordLength; i ++){
-  password += passwordOption.indexOf()
-
+  password += passwordOption.charAt(Math.floor(Math.random()*passwordOption.length))
   }
+  return password
 }
 
 
